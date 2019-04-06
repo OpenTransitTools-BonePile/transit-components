9acd1c627d30d3b787dc07fda7cfcba5e9b0e542
Frank Purcell <fxpurcell@gmail.com>
2019-04-05 17:55:37 -0700

    add config.yml file

--- map/TransitMap.jsx
+++ map/TransitMap.jsx
@@ -4,8 +4,10 @@ import { Map, TileLayer } from 'react-leaflet';
 import Control from 'react-leaflet-control';
 import BaseLayerControl from './BaseLayerControl.jsx';
 
-const mapCenter = [39.9528, -75.1638];
-const zoomLevel = 12;
+import config from "json-loader!yaml-loader!../common/config.yml";
+
+const mapCenter = [config.map.initLat, config.map.initLon];
+const zoomLevel = config.map.initZoom;
 
 const xstamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a' +
 ' href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
@@ -14,13 +16,16 @@ const xstamenXTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y
 /*
  TODO:
    - switch between tilesets
-   a) configuration + default point and zoom, etc...
+   - configuration + default point and zoom, etc...
    - make a layer control
-   c) make a pan control
+   a) HtmlWebpackPlugin({ with index templ ala MOD and index.html
+   b) refactor config so that we're not hard-coded to use ../common/config.yml
+   c) allow config to be injected into this class some way ... maybe via a test app
    d) add search
    e) add overlays (routes)
    f) add vehicles (separate component lib)
    g) localize
+   h) make a pan control
 */
 
 class TransitMap extends React.Component {
