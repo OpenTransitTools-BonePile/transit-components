975a1fda7c08036a850da2d62910cfdc7e727fa9
Frank Purcell <fxpurcell@gmail.com>
2019-05-08 22:30:04 -0700

    add prep react script 1 of 2

---
+++ scripts/prep_react_16.sh
@@ -0,0 +1,17 @@
+if [ -d "node_modules" ]; then
+    if [ -d "nm_15" ]; then
+        echo "There's already an nm_15 directory !!!"
+        echo "exiting..."
+        exit -1
+    fi
+
+    mv ./node_modules ./nm_15
+fi
+
+
+if [ -d "./nm_16" ]; then
+  mv nm_16 node_modules
+else
+  rm yarn.lock
+  yarn install
+fi
