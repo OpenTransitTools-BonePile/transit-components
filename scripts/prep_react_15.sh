bd66c8ef3786e694c5294520114eb39526edf8dc
Frank Purcell <fxpurcell@gmail.com>
2019-05-22 01:03:43 -0700

    update prep scripts to check in/out

--- scripts/prep_react_15.sh
+++ scripts/prep_react_15.sh
@@ -4,15 +4,18 @@
 echo "PREP for React 15"
 
 if [ -d "node_modules" ]; then
-    if [ -d "nm_16" ]; then
-        echo "There's already an nm_16 directory !!!"
-        echo "exiting..."
-        exit -1
-    fi
+  if [ -d "nm_16" ]; then
+    echo "There's already an nm_16 directory !!!"
+    echo "exiting..."
+    exit -1
+  fi
 
-    mv ./node_modules ./nm_16
+  mv ./node_modules ./nm_16
+  rm -rf dist/*
 fi
 
+# react-15 branch holds React 15 junk
+git checkout react-15
 
 if [ -d "./nm_15" ]; then
   mv nm_15 node_modules
--- scripts/prep_react_16.sh
+++ scripts/prep_react_16.sh
@@ -4,14 +4,18 @@
 echo "PREP for React 16"
 
 if [ -d "node_modules" ]; then
-    if [ -d "nm_15" ]; then
-        echo "There's already an nm_15 directory !!!"
-        echo "exiting..."
-        exit -1
-    fi
+  if [ -d "nm_15" ]; then
+    echo "There's already an nm_15 directory !!!"
+    echo "exiting..."
+    exit -1
+  fi
 
-    mv ./node_modules ./nm_15
+  mv ./node_modules ./nm_15
+  rm -rf dist/*
 fi
+
+# master branch holds React 16 junk
+git checkout master
 
 
 if [ -d "./nm_16" ]; then
