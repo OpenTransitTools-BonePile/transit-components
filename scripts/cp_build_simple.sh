9b22326ab147d31b62865f999b6f5f0c17cc8319
Frank Purcell <fxpurcell@gmail.com>
2019-05-30 16:56:59 -0700

    add simple build script copy to ../trimet-mod-

---
+++ scripts/cp_build_simple.sh
@@ -0,0 +1,3 @@
+rm -r ../trimet-mod-otp/node_modules/transit-components/build
+cp -r build ../trimet-mod-otp/node_modules/transit-components/
+ls -l ../trimet-mod-otp/node_modules/transit-components/
