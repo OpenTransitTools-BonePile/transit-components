af94d6e888bdc19e6d53610752923d61c8c1b565
Frank Purcell <fxpurcell@gmail.com>
2019-05-30 20:51:04 -0700

    add OTP and MOD dirs

--- scripts/cp_build_simple.sh
+++ scripts/cp_build_simple.sh
@@ -1,3 +1,7 @@
-rm -r ../trimet-mod-otp/node_modules/transit-components/build
-cp -r build ../trimet-mod-otp/node_modules/transit-components/
-ls -l ../trimet-mod-otp/node_modules/transit-components/
+MOD=../trimet-mod-otp/node_modules/transit-components/
+OTP=../otp-react-redux/node_modules/transit-components/
+
+rm -r $MOD/build $OTP/build
+cp -r build $MOD
+cp -r build $OTP
+ls -l $MOD $OTP
