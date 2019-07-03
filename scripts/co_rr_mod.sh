46ae2df37d83b10dcd512d48e04cbdf93eccce56
Frank Purcell <fxpurcell@gmail.com>
2019-07-03 10:38:37 -0700

    fix ibi / otp-rr typo screwing up script

--- scripts/co_rr_mod.sh
+++ scripts/co_rr_mod.sh
@@ -2,7 +2,7 @@ function checkout_update () {
   R=$1
   B=$2
   cd ../$R/
-  echo co up $B
+  echo co up $R $B
   git checkout $B 
   git pull upstream $B
   git push
@@ -35,11 +35,12 @@ function update_forked_repo () {
   checkout_update $R master
 }
 
+# sync TM fork of OTP with IBI's fork
+update_forked_repo OpenTripPlanner https://github.com/ibi-group/OpenTripPlanner.git
 
-# sync TM fork of OTP-RR with oritinal otp repo (master and dev branches)
+# sync TM fork of OTP-RR with IBI's fork
 update_forked_repo otp-react-redux https://github.com/opentripplanner/otp-react-redux.git
 checkout_update otp-react-redux dev
 
-
-# sync TM fork of MOD with original ibi repo (master branch)
+# sync TM fork of MOD with IBI's fork (current master / main repo for trimet-OTP-RR)
 update_forked_repo trimet-mod-otp  https://github.com/ibi-group/trimet-mod-otp.git
